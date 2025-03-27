51f1ac55e443e61da021d835900238d5f65eb384
Shahd Ahmed Mohamed kamal <147663642+ShahdAhmed2003@users.noreply.github.com>
2025-03-27 06:00:05 +0200

    Delete Entities directory

--- Entities/Customer.java
+++
@@ -1,7 +0,0 @@
-package Entities;
-
-public class Customer extends User {
-    public Customer(String id, String name, String password) {
-        super(id, name, password, "Customer"); // Pass "Customer" as the type
-    }
-}
